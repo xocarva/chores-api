a331c8629174eb924dbebd41e27141f4fecbbdfb
xocarva <correo@xoancarneiro.com>
2024-02-23 19:39:12 +0100

    use isAuthorized middleware in spaces router

--- src/api/v1/routes/spaces.router.ts
+++ src/api/v1/routes/spaces.router.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { createSpace, deleteSpace, getSpace, listSpaces, updateSpace } from '../controllers';
+import { isAuthorized } from '../../../middlewares';
 
 export const spacesRouter = express.Router();
 
-spacesRouter.get('/:id', getSpace);
-spacesRouter.get('/', listSpaces);
-spacesRouter.post('/', createSpace);
-spacesRouter.patch('/:id', updateSpace);
-spacesRouter.delete('/:id', deleteSpace);
+spacesRouter.get('/:id', isAuthorized, getSpace);
+spacesRouter.get('/', isAuthorized, listSpaces);
+spacesRouter.post('/', isAuthorized, createSpace);
+spacesRouter.patch('/:id', isAuthorized, updateSpace);
+spacesRouter.delete('/:id', isAuthorized, deleteSpace);
