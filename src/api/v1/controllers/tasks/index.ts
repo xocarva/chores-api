85334595eedc57e0f07abe1868e97133b6596f34
xocarva <correo@xoancarneiro.com>
2024-02-24 10:48:12 +0100

    remove list tasks controller

--- src/api/v1/controllers/tasks/index.ts
+++ src/api/v1/controllers/tasks/index.ts
@@ -1,5 +1,4 @@
 export * from './createTask.controller';
 export * from './deleteTask.controller';
 export * from './getSpace.controller';
-export * from './listTasks.controller';
 export * from './updateTask.controller';
