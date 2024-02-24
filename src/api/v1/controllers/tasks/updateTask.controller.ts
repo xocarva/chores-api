358d0cab3f9461da2b91269164fdc3f056cc6803
xocarva <correo@xoancarneiro.com>
2024-02-24 10:50:47 +0100

    fix validation

--- src/api/v1/controllers/tasks/updateTask.controller.ts
+++ src/api/v1/controllers/tasks/updateTask.controller.ts
@@ -1,13 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 import { tasksService } from '../../services';
-import { TaskWithId, partialSpaceSchema, routeParamIdSchema } from '../../schemas';
+import { TaskWithId, partialTaskSchema, routeParamIdSchema } from '../../schemas';
 
 export async function updateTask(req: Request, res: Response<{ task: TaskWithId }>, next: NextFunction) {
   const { body } = req;
   
   try {
     const { id } = routeParamIdSchema.parse(req.params);
-    const updateData = partialSpaceSchema.parse(body);
+    const updateData = partialTaskSchema.parse(body);
     const task = await tasksService.updateOne(id, updateData);
 
     res.status(200);
