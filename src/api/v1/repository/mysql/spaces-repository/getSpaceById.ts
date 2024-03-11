2570ef2c602976330d7b57637b0533b25093d9d3
xocarva <correo@xoancarneiro.com>
2024-03-11 17:46:54 +0100

    retrieve name in getSpace

--- src/api/v1/repository/mysql/spaces-repository/getSpaceById.ts
+++ src/api/v1/repository/mysql/spaces-repository/getSpaceById.ts
@@ -4,10 +4,10 @@ import { SpaceWithId, User } from '../../../schemas';
 
 type UserRow = User & RowDataPacket;
 
-export async function getSpaceById(id: number): Promise<SpaceWithId | null> {
+export async function getSpaceById(spaceId: number): Promise<SpaceWithId | null> {
   const [spaces] = await pool.query<RowDataPacket[]>(
     'SELECT id, title, description FROM spaces WHERE id = ?',
-    [id],
+    [spaceId],
   );
 
   if (spaces.length === 0) {
@@ -17,15 +17,18 @@ export async function getSpaceById(id: number): Promise<SpaceWithId | null> {
   const space = spaces[0] as SpaceWithId;
 
   const [users] = await pool.query<UserRow[]>(
-    'SELECT user_id as id, admin FROM user_spaces WHERE space_id = ?',
+    'SELECT u.id, u.name, us.admin FROM user_spaces us ' +
+    'INNER JOIN users u ON us.user_id = u.id ' +
+    'WHERE us.space_id = ?',
     [space.id],
   );
 
   return { 
     ...space, 
-    users: users.map(user => ({
-      id: user.id, 
-      admin: user.admin ?? false,
+    users: users.map(({ id, name, admin }) => ({
+      id, 
+      name,
+      admin: admin ?? false,
     })),
   };
 }
